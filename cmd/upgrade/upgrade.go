c9b468f33cd73eb25a50fd363198b46341f7e446
Arvind Iyengar <arvind.iyengar@rancher.com>
2021-03-09 10:22:23 -0800

    Remove binary option and remove --register from wins-args

--- cmd/upgrade/upgrade.go
+++ cmd/upgrade/upgrade.go
@@ -5,11 +5,21 @@ import (
 	"os"
 	"strings"
 
-	"github.com/pkg/errors"
 	"github.com/rancher/wins/cmd/upgrade/internal/powershell"
 	"github.com/sirupsen/logrus"
 	"github.com/urfave/cli"
 )
+
+var _upgradeFlags = []cli.Flag{
+	cli.StringFlag{
+		Name:  "wins-args",
+		Usage: "[optional] Arguments to pass onto wins srv app run --register",
+	},
+	cli.BoolFlag{
+		Name:  "debug",
+		Usage: "[optional] whether to print debugging logs from performing the upgrade",
+	},
+}
 
 const upgradePS1Fmt = `& {
 	$ErrorActionPreference = "Stop";
@@ -64,41 +74,14 @@ const restartServicePS1 = `& {
 	}
 }`
 
-var _upgradeFlags = []cli.Flag{
-	cli.StringFlag{
-		Name:  "binary",
-		Usage: "[optional] Name of binary in working directory to upgrade to",
-		Value: os.Args[0], // default behavior is to use current binary
-	},
-	cli.StringFlag{
-		Name:  "wins-args",
-		Usage: "[optional] Arguments to pass onto wins srv app run",
-		Value: "--register",
-	},
-	cli.BoolFlag{
-		Name:  "debug",
-		Usage: "[optional] whether to print debugging logs from performing the upgrade",
-	},
-}
-
 func _upgradeRequestParser(cliCtx *cli.Context) (err error) {
 	// validate
-	binPath := cliCtx.String("binary")
-	stat, err := os.Stat(binPath)
-	if err != nil {
-		if !os.IsNotExist(err) {
-			return errors.Wrapf(err, "cannot find --binary %s", binPath)
-		}
-		return nil
-	} else if stat.IsDir() {
-		return errors.New("expected file for binary, found directory")
-	}
 	winsArgs := cliCtx.String("wins-args")
 	if strings.Contains(winsArgs, "--unregister") {
 		return fmt.Errorf(`cannot provide "--unregister" to --wins-args`)
 	}
 	if !strings.Contains(winsArgs, "--register") {
-		return fmt.Errorf(`must provide a string containing "--register" to --win-args`)
+		return fmt.Errorf(`cannot provide "--register" to --wins-args`)
 	}
 	return nil
 }
@@ -107,9 +90,8 @@ func _upgradeAction(cliCtx *cli.Context) (err error) {
 	if cliCtx.Bool("debug") {
 		logrus.SetLevel(logrus.DebugLevel)
 	}
-	binPath := cliCtx.String("binary")
+	binPath := os.Args[0]
 	winsArgs := cliCtx.String("wins-args")
-	winsArgs = strings.Replace(winsArgs, "--register", "", 1)
 	out, err := powershell.RunCommandf(upgradePS1Fmt, binPath, winsArgs)
 	if len(out) > 0 {
 		logrus.Debugf("logs from upgrade.ps1 \n%s\nEOF", out)
