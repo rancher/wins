e10c48303d57733c6405e625987b432fa3c484ad
Arvind Iyengar <arvind.iyengar@rancher.com>
2021-03-10 15:37:07 -0800

    Drop HOST_RELEASE_ID stuff

--- scripts/package.ps1
+++ scripts/package.ps1
@@ -26,29 +26,17 @@ if ($env:DRONE_TAG) {
 }
 
 # Get release id as image tag suffix
-$HOST_RELEASE_ID = (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\' -ErrorAction Ignore).ReleaseId
 $RELEASE_ID = $env:RELEASE_ID
-if (-not $RELEASE_ID) {
-    $RELEASE_ID = $HOST_RELEASE_ID
-}
 $IMAGE = ('{0}/wins:{1}-windows-{2}' -f $REPO, $TAG, $RELEASE_ID)
 
 $ARCH = $env:ARCH
-if ($RELEASE_ID -eq $HOST_RELEASE_ID) {
-    docker build `
-        --build-arg SERVERCORE_VERSION=$RELEASE_ID `
-        --build-arg ARCH=$ARCH `
-        --build-arg VERSION=$TAG `
-        -t $IMAGE `
-        -f Dockerfile .
-} else {
-    docker build `
-        --isolation hyperv `
-        --build-arg SERVERCORE_VERSION=$RELEASE_ID `
-        --build-arg ARCH=$ARCH `
-        --build-arg VERSION=$TAG `
-        -t $IMAGE `
-        -f Dockerfile .
-}
+
+docker build `
+    --isolation hyperv `
+    --build-arg SERVERCORE_VERSION=$RELEASE_ID `
+    --build-arg ARCH=$ARCH `
+    --build-arg VERSION=$TAG `
+    -t $IMAGE `
+    -f Dockerfile .
 
 Write-Host "Built $IMAGE`n"
