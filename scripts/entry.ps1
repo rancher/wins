207ef63d936fa2cee1ac71a263aedb6cda64215f
aiyengar2 <iyengararvind@gmail.com>
2021-03-12 15:41:51 -0800

    Fix Drone CI (#27)

--- scripts/entry.ps1
+++ scripts/entry.ps1
@@ -13,8 +13,8 @@ $SCRIPT_PATH = ("{0}\{1}.ps1" -f $PSScriptRoot, $Args[0])
 if (Test-Path $SCRIPT_PATH -ErrorAction Ignore)
 {
     Invoke-Expression -Command $SCRIPT_PATH
-    if (-not $?) {
-        exit 255
+    if ($LASTEXITCODE -ne 0) {
+        exit $LASTEXITCODE
     }
     exit 0
 }
