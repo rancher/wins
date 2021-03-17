719af8b44c82d6d9c07d0aa03f1c1ea453668a0a
Arvind Iyengar <arvind.iyengar@rancher.com>
2021-03-17 16:45:54 -0700

    Add random args to nginx

--- tests/integration/docker/nginx.ps1
+++ tests/integration/docker/nginx.ps1
@@ -4,4 +4,4 @@ $ErrorActionPreference = "Stop"
 Copy-Item -Recurse -Force -Path c:\etc\nginx\* -Destination c:\host\etc\nginx\ | Out-Null
 
 # start process
-wins.exe cli prc run --path c:\etc\nginx\nginx.exe --exposes TCP:80
+wins.exe cli prc run --path c:\etc\nginx\nginx.exe --exposes TCP:80 --args=`-g env hello=world`
