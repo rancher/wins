3209c534ded8de835b3855f16f376355bb679dd1
Arvind Iyengar <arvind.iyengar@rancher.com>
2021-03-17 17:02:11 -0700

    Add integration tests to ci workflow
    (cherry picked from commit 26c94f3e0534c787760288f44d40b8d2350e5560)

--- scripts/ci.ps1
+++ scripts/ci.ps1
@@ -4,3 +4,4 @@ Import-Module -WarningAction Ignore -Name "$PSScriptRoot\utils.psm1"
 
 Invoke-Script -File "$PSScriptRoot\test.ps1"
 Invoke-Script -File "$PSScriptRoot\build.ps1"
+Invoke-Script -File "$PSScriptRoot\integration.ps1"
