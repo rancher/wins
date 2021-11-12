16dfc3b59295430097b9e85697aeec970f14c11b
Jamie Phillips <jamie.phillips@suse.com>
2021-11-12 12:27:33 -0500

    Adding Windows Server 2022 support.
    Signed-off-by: Jamie Phillips <jamie.phillips@suse.com>

--- scripts/package.ps1
+++ scripts/package.ps1
@@ -29,17 +29,19 @@ if ($env:DRONE_TAG) {
     $TAG = $env:DRONE_TAG
 }
 
-# Get release id as image tag suffix
-$HOST_RELEASE_ID = (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\' -ErrorAction Ignore).ReleaseId
-$IMAGE = ('{0}/wins:{1}-windows-{2}' -f $REPO, $TAG, $HOST_RELEASE_ID)
-if (-not $HOST_RELEASE_ID) {
-    Log-Fatal "release ID not found"
+$buildTags = @{ "17763" = "1809"; "19042" = "20H2"; "20348" = "ltsc2022";}
+$buildNumber = (Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\' -ErrorAction Ignore).CurrentBuildNumber
+$WINDOWS_VERSION = $buildTags[$buildNumber]
+if (-not $WINDOWS_VERSION) {
+    $WINDOWS_VERSION = "1809"
 }
+
+$IMAGE = ('{0}/wins:{1}-windows-{2}' -f $REPO, $TAG, $WINDOWS_VERSION)
 
 $ARCH = $env:ARCH
 
 docker build `
-    --build-arg SERVERCORE_VERSION=$HOST_RELEASE_ID `
+    --build-arg SERVERCORE_VERSION=$WINDOWS_VERSION `
     --build-arg ARCH=$ARCH `
     --build-arg VERSION=$TAG `
     -t $IMAGE `
