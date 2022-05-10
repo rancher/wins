f3b5def4971dbe773b3a2b5b2bd152b7e87de231
Jamie Phillips <jamie.phillips@suse.com>
2022-05-10 13:32:22 -0400

    Fixing the issue with TLS Config settings for CSI Proxy.

--- pkg/tls/tls.go
+++ pkg/tls/tls.go
@@ -15,9 +15,9 @@ type Config struct {
 }
 
 // SetupGenericTLSConfigFromFile returns a x509 system certificate pool containing the specified certificate file
-func SetupGenericTLSConfigFromFile() (*x509.CertPool, error) {
-	var config *Config
-	if config.CertFilePath == "" {
+func (c *Config) SetupGenericTLSConfigFromFile() (*x509.CertPool, error) {
+	logrus.Infof("Configuring TLS as insecure: %v and using the following cert: %s", c.Insecure, c.CertFilePath)
+	if c.CertFilePath == "" {
 		logrus.Info("[SetupGenericTLSConfigFromFile] specified certificate file path is empty, not modifying system certificate store")
 		return nil, nil
 	}
@@ -29,21 +29,21 @@ func SetupGenericTLSConfigFromFile() (*x509.CertPool, error) {
 		systemCerts = x509.NewCertPool()
 	}
 
-	localCertPath, err := os.Stat(config.CertFilePath)
+	localCertPath, err := os.Stat(c.CertFilePath)
 	if err != nil {
-		return nil, fmt.Errorf("[SetupGenericTLSConfigFromFile] unable to read certificate %s from %s: %v", localCertPath.Name(), config.CertFilePath, err)
+		return nil, fmt.Errorf("[SetupGenericTLSConfigFromFile] unable to read certificate %s from %s: %v", localCertPath.Name(), c.CertFilePath, err)
 	}
 
-	certs, err := ioutil.ReadFile(config.CertFilePath)
+	certs, err := ioutil.ReadFile(c.CertFilePath)
 	if err != nil {
-		return nil, fmt.Errorf("[SetupGenericTLSConfigFromFile] failed to read local cert file %q: %v", config.CertFilePath, err)
+		return nil, fmt.Errorf("[SetupGenericTLSConfigFromFile] failed to read local cert file %q: %v", c.CertFilePath, err)
 	}
 
 	// Append the specified cert to the system pool
 	if ok := systemCerts.AppendCertsFromPEM(certs); !ok {
-		return nil, fmt.Errorf("[SetupGenericTLSConfigFromFile] unable to append cert %s to store, using system certs only", config.CertFilePath)
+		return nil, fmt.Errorf("[SetupGenericTLSConfigFromFile] unable to append cert %s to store, using system certs only", c.CertFilePath)
 	}
 
-	logrus.Infof("[SetupGenericTLSConfigFromFile] successfully loaded %s certificate into system cert store", config.CertFilePath)
+	logrus.Infof("[SetupGenericTLSConfigFromFile] successfully loaded %s certificate into system cert store", c.CertFilePath)
 	return systemCerts, nil
 }
