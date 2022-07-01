8894e990c5eaf04f19c12a178e886bb3d8c812c6
Ross Kirkpatrick <ross.kirkpatrick@suse.com>
2022-07-01 15:41:58 -0400

    uncomment test scripts

--- scripts/ci.ps1
+++ scripts/ci.ps1
@@ -7,12 +7,12 @@ Import-Module -WarningAction Ignore -Name "$PSScriptRoot\utils.psm1"
 try {
     if (-not ($SKIP_VALIDATE)) {
         Write-Host "Invoking validate.ps1"
-        #    Invoke-Script -File "$PSScriptRoot\validate.ps1"
+        Invoke-Script -File "$PSScriptRoot\validate.ps1"
     }
 
     if (-not ($SKIP_TESTS)) {
-    Write-Host "Invoking test.ps1"
-    #    Invoke-Script -File "$PSScriptRoot\test.ps1"
+        Write-Host "Invoking test.ps1"
+        Invoke-Script -File "$PSScriptRoot\test.ps1"
     }
 
     Write-Host "Invoking build.ps1"
