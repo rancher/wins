9d9062332ad70ea92121e49da2ee1ea766b0cc33
Ross Kirkpatrick <ross@rancher.com>
2022-08-18 15:36:42 -0400

    improve logic and logging (#141)
    * improve logic and logging
    
    * fix eventid not being set

--- suc/run.ps1
+++ suc/run.ps1
@@ -24,11 +24,12 @@ function New-Directory {
     if (Test-Path -Path $Path) {
         if (-not (Test-Path -Path $Path -PathType Container)) {
             # clean the same path file
+            Invoke-LogWrite -LogName Application -Source rancher-wins -Message "Existing path found on host, recursively cleaning $($Path)"
             Remove-Item -Recurse -Force -Path $Path -ErrorAction Ignore | Out-Null
         }
         return
     }
-
+    Invoke-LogWrite -LogName Application -Source rancher-wins -Message "Creating $($Path)"
     New-Item -Force -ItemType Directory -Path $Path | Out-Null
 }
 
@@ -57,6 +58,7 @@ function Start-TransferFile {
     }
 
     Copy-Item -Force -Path $Source -Destination $Destination | Out-Null
+    Invoke-LogWrite -LogName Application -Source rancher-wins -Message "Transferred file to $Destination from $Source"
 }
 
 function Invoke-WinsHostProcessUpgrade {
@@ -66,7 +68,9 @@ function Invoke-WinsHostProcessUpgrade {
     New-Directory -Path $tmpdir
 
     Start-TransferFile -Source "C:\wins.exe" -Destination  $tmpdir
+    Invoke-LogWrite -LogName Application -Source rancher-wins -Message "Transferred C:\wins.exe to $($tmpdir)"
     Start-TransferFile -Source "C:\install.ps1" -Destination $tmpdir
+    Invoke-LogWrite -LogName Application -Source rancher-wins -Message "Transferred C:\install.ps1 to $($tmpdir)"
 
     if (-Not $env:CATTLE_ROLE_WORKER) {
         $env:CATTLE_ROLE_WORKER = "true"
@@ -77,13 +81,26 @@ function Invoke-WinsHostProcessUpgrade {
 
     Set-Location -Path $tmpdir
 
+    Invoke-LogWrite -LogName Application -Source rancher-wins -Message "Running install.ps1 in $($tmpdir)"
     ./install.ps1
 
     Pop-Location
 
+    Invoke-LogWrite -LogName Application -Source rancher-wins -Message "Successfully ran install.ps1, cleaning $($tmpdir)\wins.exe"
     Remove-Item -Force -Path $tmpdir\wins.exe -ErrorAction Ignore | Out-Null
+    Invoke-LogWrite -LogName Application -Source rancher-wins -Message "Successfully ran install.ps1, cleaning $($tmpdir)\install.ps1"
     Remove-Item -Force -Path $tmpdir\install.ps1 -ErrorAction Ignore | Out-Null
     exit 0
+}
+
+function Invoke-LogWrite {
+    param (
+        [parameter(Mandatory = $true)] [string]$Message,
+        [parameter(Mandatory = $true)] [string]$LogName,
+        [parameter(Mandatory = $true)] [string]$Source
+    )
+    Write-Host "$($Message)"
+    Write-EventLog -LogName "$LogName" -Source "$Source" -Message "$Message" -EID 1
 }
 
 function Invoke-WinsWinsUpgrade {
@@ -94,38 +111,42 @@ function Invoke-WinsWinsUpgrade {
     $winsUpgradePathLocal = Join-Path -Path $tmpdirLocal -ChildPath "wins-upgrade.exe"
 
     New-Directory -Path $tmpdirLocal
+    Invoke-LogWrite -LogName Application -Source rancher-wins -Message "Created new directory $($tmpdirLocal)"
     Copy-Item -Force -Path "C:\wins.exe" -Destination $winsUpgradePathLocal | Out-Null
+    Invoke-LogWrite -LogName Application -Source rancher-wins -Message "Copied C:\wins.exe to $($winsUpgradePathLocal)"
 
-    Write-Host "Transferring file to host..."
+    Invoke-LogWrite -LogName Application -Source rancher-wins -Message "Transferring file to host..."
     New-Directory -Path $winsUpgradePath
+    Invoke-LogWrite -LogName Application -Source rancher-wins -Message "Created new directory $($winsUpgradePath)"
     Start-TransferFile -Source "C:\wins.exe" -Destination $winsUpgradePath
 
-    Write-Host "Checking if $($winsUpgradePath) exists"
+    Invoke-LogWrite -LogName Application -Source rancher-wins -Message "Checking if $($winsUpgradePath) exists"
     if(Test-Path $winsUpgradePath) {
-        Write-Host "$($winsUpgradePath) exists..."
+        Invoke-LogWrite -LogName Application -Source rancher-wins -Message "$($winsUpgradePath) exists..."
     }
     else {
-        Write-Host "$($winsUpgradePath) was not copied to host..."
+        Invoke-LogWrite -LogName Application -Source rancher-wins -Message "$($winsUpgradePath) was not copied to host..."
         exit 1
     }
+    Invoke-LogWrite -LogName Application -Source rancher-wins -Message "preparing to run wins.exe upgrade using $($winsUpgradePathLocal)"
 
     $winsOut = wins.exe cli prc run --path=$winsUpgradePathLocal --args="up"
 
     Remove-Item -Recurse -Force -Path $winsUpgradePath -ErrorAction Ignore | Out-Null
 
 
-    if ($winsOut -match ".* rpc error: code = Unavailable desc = transport is closing") {
-        Write-Host "Successfully upgraded"
+    if ($winsOut -match ".* rpc error: code = Unavailable desc = transport is closing" -or ".* rpc error: code = Unavailable desc = error reading from server: EOF") {
+        Invoke-LogWrite -LogName Application -Source rancher-wins -Message "Successfully upgraded"
         exit 0
     }
     elseif ($LastExitCode -ne 0) {
-        Write-Host "Returned exit $LastExitCode"
-        Write-Host $winsOut
+        Invoke-LogWrite -LogName Application -Source rancher-wins -Message "Returned exit $LastExitCode"
+        Invoke-LogWrite -LogName Application -Source rancher-wins -Message $winsOut
         exit $LastExitCode
     }
     else {
-        Write-Host "Returned exit 0, but did not receive expected output from .\wins up"
-        Write-Host $winsOut
+        Invoke-LogWrite -LogName Application -Source rancher-wins -Message "Returned exit 0, but did not receive expected output from .\wins up"
+        Invoke-LogWrite -LogName Application -Source rancher-wins -Message $winsOut
         exit 1
     }  
 }
