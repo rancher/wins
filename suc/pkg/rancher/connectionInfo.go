e36cf80e45382cbc7c3f3ea6b9eda5995e7dd140
Harrison Affel <harrisonaffel@gmail.com>
2024-09-27 11:21:28 -0400

    Refactor SUC

--- suc/pkg/rancher/connectionInfo.go
+++ suc/pkg/rancher/connectionInfo.go
@@ -11,13 +11,13 @@ import (
 
 const connInfoScriptName = "/hpc/update-connection-info.ps1"
 
-func UpdateConnectionInformation() (string, error) {
+func UpdateConnectionInformation() (string, bool, error) {
 	_, err := os.Stat(connInfoScriptName)
 	if errors.Is(err, os.ErrNotExist) {
-		logrus.Errorf("Could not find %s, will not attempt to update Rancher connection information.", connInfoScriptName)
-		return "", nil
+		logrus.Warnf("Could not find %s, will not attempt to update Rancher connection information.", connInfoScriptName)
+		return "", false, nil
 	} else if err != nil {
-		return "", fmt.Errorf("failed to open %s: %w", connInfoScriptName, err)
+		return "", false, fmt.Errorf("failed to open %s: %w", connInfoScriptName, err)
 	}
 
 	// This command is expected to be run in a host process pod. Files packaged into
@@ -25,7 +25,7 @@ func UpdateConnectionInformation() (string, error) {
 	cmd := exec.Command("powershell", "-File", connInfoScriptName)
 	o, err := cmd.CombinedOutput()
 	if err != nil {
-		return string(o), fmt.Errorf("failed to update connection info: %w", err)
+		return string(o), true, fmt.Errorf("failed to update connection info: %w", err)
 	}
-	return string(o), nil
+	return string(o), true, nil
 }
