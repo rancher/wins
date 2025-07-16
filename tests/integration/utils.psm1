e51a48db0f35fcc6cd7093dc2d3022976202ad4e
Harrison Affel <harrisonaffel@gmail.com>
2025-07-16 13:19:15 -0400

    fix: properly handle certificate chains in install.ps1

--- tests/integration/utils.psm1
+++ tests/integration/utils.psm1
@@ -349,6 +349,138 @@ function Get-LatestCommitOrTag {
     return $(git rev-parse --short HEAD)
 }
 
+function Cleanup-CertFile() {
+    $path = "$PSScriptRoot\testcert.pem"
+    Remove-Item -Path $path -Force -ErrorAction Ignore
+}
+
+function Setup-CertFiles() {
+    param (
+        [Parameter()]
+        [int]
+        $length
+    )
+
+    $certFilePath = "$PSScriptRoot\testcert.pem"
+
+    Log-Info "Generating $length certificates"
+
+    $root = New-SelfSignedCertificate -Subject "CN=wins-test-root-CA" -KeyUsage CertSign, CRLSign -KeyAlgorithm RSA -KeyLength 2048 -NotAfter (Get-Date).AddYears(1)
+    if ($null -eq $root) {
+        Log-Info "Failed to generate root certificate"
+        exit 1
+    }
+
+    Log-Info "Generated root CA certificate"
+
+    [System.Security.Cryptography.X509Certificates.X509Certificate2[]]$certChain = @()
+    $certChain += ($root)
+    # Subtract 1 since we always include the root CA
+    for ($i = 0; $i -lt $length-1; $i++) {
+        if ($i -eq $length - 2) {
+            $cert = newCert -parentCerts $certChain -isLeaf
+            if ($null -ne $cert)
+            {
+                $certChain += ($cert)
+            }
+        } else
+        {
+            $cert = newCert -parentCerts $certChain -isIntermediate
+            if ($null -ne $cert)
+            {
+                $certChain += ($cert)
+            }
+        }
+    }
+
+    $finalCertFile = ""
+    Log-Info "Generated $length certificates"
+    foreach ($cert in $certChain) {
+        $block = getCertString -cert $cert
+        $finalCertFile = $finalCertFile.Trim() + "`n" + $block.Trim()
+    }
+
+    # write final cert file to disk to generate a hash
+    Set-Content -NoNewline -Path $certFilePath -Value $finalCertFile
+
+    $thumbPrints = @()
+
+    Log-Info "Removing all generated certs from local cert store"
+    foreach ($cert in $certChain) {
+        $thumbPrints += @($cert.Thumbprint)
+        Remove-Item -Path $cert.PSPath -DeleteKey
+    }
+
+    Log-Info "Deleted all certs from cert stores"
+    $shasum = (Get-FileHash -Path $certFilePath -Algorithm SHA256).Hash.ToLower()
+    Log-Info "Computed sha256 sum of the final cert file is: $shasum"
+
+    return [PSCustomObject]@{
+        FinalCertBlocks = $finalCertFile
+        ThumbPrints = $thumbPrints
+        Checksum = $shasum
+    }
+}
+
+function getCertString() {
+    param (
+        [Parameter()]
+        [System.Security.Cryptography.X509Certificates.X509Certificate2]
+        $cert
+    )
+
+    if ($cert -eq $null) {
+        Log-Error "a null cert was provided to getCertString"
+        exit 1
+    }
+
+    $base64Content = [System.Convert]::ToBase64String($cert.RawData, 'InsertLineBreaks')
+    $newBlock = @"
+-----BEGIN CERTIFICATE-----
+$base64Content
+-----END CERTIFICATE-----
+"@
+
+    return $newBlock
+}
+
+function newCert() {
+    param (
+        [Parameter()]
+        [System.Security.Cryptography.X509Certificates.X509Certificate2[]]
+        $parentCerts,
+
+        [Parameter()]
+        [Switch]
+        $isLeaf,
+
+        [Parameter()]
+        [Switch]
+        $isIntermediate
+    )
+
+    if ($isLeaf -and $isIntermediate) {
+        Log-Error "a cert cannot be both a leaf and an intermediate"
+        return
+    }
+
+    if ($isLeaf) {
+        Log-Info "Creating a new leaf certificate"
+        $cert = New-SelfSignedCertificate -Subject "CN=wins-test-leaf-cert" -DnsName "wins.com" -Signer $parentCerts[-1] -KeyAlgorithm RSA -KeyLength 2048 -NotAfter (Get-Date).AddYears(1)
+        return $cert
+    }
+
+    if ($isIntermediate) {
+        Log-Info "Creating a new intermediate certificate"
+        $cert = New-SelfSignedCertificate -Subject "CN=wins-test-intermediate-cert" -KeyUsage CertSign, CRLSign -Signer $parentCerts[-1] -KeyAlgorithm RSA -KeyLength 2048 -NotAfter (Get-Date).AddYears(1)
+        return $cert
+    }
+
+    return $null
+}
+
+Export-ModuleMember -Function Setup-CertFiles
+Export-ModuleMember -Function Cleanup-CertFile
 Export-ModuleMember -Function Log-Info
 Export-ModuleMember -Function Log-Warn
 Export-ModuleMember -Function Log-Error
