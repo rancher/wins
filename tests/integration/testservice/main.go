4224c195d390dba7e9c53e9c4bb84c4e85483ee2
Vardhaman <vardhaman.surana@suse.com>
2026-07-07 23:53:03 +0530

    Uninstall script improvements (#336)

---
+++ tests/integration/testservice/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"golang.org/x/sys/windows/svc"
+)
+
+type handler struct{}
+
+func (h *handler) Execute(
+	args []string,
+	requests <-chan svc.ChangeRequest,
+	status chan<- svc.Status,
+) (bool, uint32) {
+
+	const accepted = svc.AcceptStop | svc.AcceptShutdown
+	status <- svc.Status{State: svc.Running, Accepts: accepted}
+
+	for req := range requests {
+		switch req.Cmd {
+		case svc.Stop, svc.Shutdown:
+			status <- svc.Status{State: svc.Stopped}
+			return false, 0
+		}
+	}
+
+	return false, 0
+}
+
+func main() {
+	isService, err := svc.IsWindowsService()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "failed to detect service context: %v\n", err)
+		os.Exit(1)
+	}
+
+	if isService {
+		serviceName := "test"
+
+		if len(os.Args) > 1 {
+			serviceName = os.Args[1]
+		}
+		if err := svc.Run(serviceName, &handler{}); err != nil {
+			fmt.Fprintf(os.Stderr, "service failed: %v\n", err)
+			os.Exit(1)
+		}
+		return
+	}
+
+	time.Sleep(15 * time.Minute)
+}
